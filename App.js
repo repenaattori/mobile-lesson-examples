f6ab26271494a26100cb79bce96ebe0fbe5186a8
repenaattori <reimarii@oamk.fi>
2023-01-17 13:21:44 +0200

    styling

--- App.js
+++ App.js
@@ -1,30 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { Button, Image, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
-import MyView from './components/MyView';
+import { Styles } from './styles/Styles';
+
 
 export default function App() {
 
-  const [text, setText] = useState('');  
-
-  const x = '0';
-
-  Number(x) + Number(x)
-
-  function pressed(){
-    console.log('Text is pressed');
-  }
-
   return (
-    <View style={styles.container}>
-      <TextInput 
-        value={text} 
-        onChangeText={(t)=> setText(t)} 
-      />
-      <Pressable onPress={pressed}>
-        <Image source={require('./assets/favicon.png')}/>
-      </Pressable>
-      <Text>You're name is {text}</Text>
+    <View style={Styles.container}>
+      <Text style={Styles.text}>First</Text>
+      <Text style={[Styles.text, {flex: 4}]}>Second</Text>
     </View>
   );
 }
@@ -32,11 +17,4 @@ export default function App() {
 
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'yellow',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ styles/Styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const Styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: 'yellow'
+    },
+    text:{
+      backgroundColor: '#ebac0e',
+      borderWidth: 1,
+      flex:1
+    }
+});
+
+export {Styles};
