199d3013af6ffeff8695b8df7cf6a699849793c3
repenaattori <reimarii@oamk.fi>
2023-01-24 13:32:40 +0200

    Small list exercise

---
+++ ListExercise.js
@@ -0,0 +1,11 @@
+//1 Tee map-funktiolla taulu, joka sisältää hinnat 60% alennuksella.
+let prices = [200, 450, 234, 430, 670, 500, 150];
+
+
+//2 Tee map-funktiolla toinen taulukko, jossa on jokaista 
+//yli 200e hintaa kohden teksti "kallis" ja lopuissa teksti "halpa"
+
+//3 Tee filter-funktiolla taulu, jossa on vain 300:aa halvemmat hinnat.
+
+//4 Suodata taulukosta vain ne nimet, joissa on kirjaimia 6 tai enemmän (length)
+let names = ["John", "Lisa", "Marco", "Elizabeht", "Gunther", "Eveliina", "Risto-Matti", "Marko", "Eijastiina"];
