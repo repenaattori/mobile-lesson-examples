7db3ca23a85eb34044f055c1513b395f9de5f560
repenaattori <reimarii@oamk.fi>
2023-01-26 10:59:17 +0200

    Adding search and select

--- App.js
+++ App.js
@@ -3,31 +3,41 @@ import { useState } from 'react';
 import { Button, FlatList, Image, Pressable, SafeAreaView, ScrollView, StyleSheet, Switch, Text, TextInput, View } from 'react-native';
 import { Styles, SpecialStyles } from './styles/Styles';
 import Constants from 'expo-constants';
-import NumericInput from 'react-native-numeric-input';
-import { RadioButton } from 'react-native-paper';
-import SelectDropdown from 'react-native-select-dropdown';
 import DATA from './Data';
 import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
 
 export default function App() {
 
   const [persons, setPersons] = useState(DATA);
+  const [text, setText] = useState('');
+  const [selectedId, setSelectedId] = useState();
+
+  function search(keyword){
+    setText(keyword);
+    const filteredPersons = DATA.filter(p => p.fname.includes(keyword));
+    setPersons(filteredPersons);
+  }
 
   return (
       <SafeAreaView style={Styles.container}>
-          <AntDesign name="pausecircle" size={60} color="red" onPress={()=>console.log("hello")}/>
+          <TextInput style={{borderWidth:1, padding: 5}} value={text} onChangeText={search}/>
           <FlatList
             data={persons}
-            renderItem={ ({item}) => <Item  item={item}/> }
+            renderItem={ ({item}) => <Item  item={item} selectedId={selectedId} select={setSelectedId}/> }
           />
       </SafeAreaView>
   );
 }
 
 
-function Item({item}){
+function Item({item, selectedId, select}){
+
+  const color = selectedId == item.id ? 'orange' : 'yellow';
+
   return(
-    <Text style={{padding: 50, borderWidth:1, margin: 5}}>{item.fname} {item.lname}</Text> 
+    <Pressable onPress={()=> select(item.id)}>
+      <Text style={{padding: 50, borderWidth:1, margin: 5, backgroundColor: color}}>{item.fname} {item.lname}</Text> 
+    </Pressable>
   );
 }
 
